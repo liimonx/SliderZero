bfca1a14f00fa75f2be05a92c445d516d4cabc22
liimonx <limonkhan669@gmail.com>
2018-10-20 04:47:40 +0600

    Make constructor & add some defaults options

--- src/index.js
+++ src/index.js
@@ -16,4 +16,43 @@ import {
   'use strict'
 
 
+  function SliderZero(elm, options) {
+    this.elm = elm
+
+    for (var prop in SliderZero.defaults) {
+      this.options[prop] = SliderZero.defaults[prop]
+    }
+
+    for (prop in options) {
+        this.options[prop] = options[prop]
+    }
+
+  }
+
+  SliderZero.defaults = {
+    autoSlide: true,
+    autoSlideTime : 3,
+    controlBar = true,
+    barClick = true,
+    barStyle = {
+      border: 0,
+      background: '#ccc',
+      barColor: 'blue'
+    }
+  }
+
+
+
+
+  SliderZero.prototype = {
+    
+  }
+
+
+
+
+  window.SliderZero = SliderZero
+
 })(window)
+
+export default SliderZero
